fd4cf2d67ec436af54eac14857da243662b7235f
gunwant07 <144123267+gunwant07@users.noreply.github.com>
2023-10-05 10:36:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,20 @@ server.post('/demo', async (req, res) => {
   console.log(doc);
   res.json(doc);
 });
-
+// Add these lines to allow cross-origin requests
+server.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With,content-type'
+  );
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 server.listen(port, () => {
   console.log('server started');
 });
